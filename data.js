a3b766f2877c3498ea6cda925be04876cbd767f7
Doyzy78 <162144547+Doyzy78@users.noreply.github.com>
2025-07-04 14:27:51 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const appData = [
+  {
+    "id": "NetMirror",
+    "name": "NetMirror",
+    "version": "3.2.",
+    "size": "150 MB",
+    "icon": "icon/mirror.png",
+    "apk": "downloads/NetMirror.apk",
+    "description": "mod.  all Feature unlock",
+    "developer": "Joker Devs",
+    "updated": "2025-07-04",
+    "downloads": "25,000+"
+  },
+  {
+    "id": "Spotify",
+    "name": "Spotify Premium",
+    "version": "9.0.58.578",
+    "size": "154 MB",
+    "icon": "icon/Spotify.png",
+    "apk": "downloads/Spotify_9.0.58.578.apk",
+    "description": "mod. premium unlock no login problem all Feature unlock",
+    "developer": "Joker Devs",
+    "updated": "2025-07-04",
+    "downloads": "25,000+"
+  }
+];
